77c239794ed2d062276b20bc7b8db6ae2da691a5
Kristof Siket <kristofsiket@kinsta.com>
2023-12-26 13:41:28 +0100

    feat(graphql): schema extended with resolvers

--- src/core/schema.ts
+++ src/core/schema.ts
@@ -49,184 +49,48 @@ export const typeDefs = gql`
 
 export const resolvers = {
   Query: {
-    allUsers: (_parent, _args, context: Context) => {
-      return context.prisma.user.findMany()
-    },
-    postById: (_parent, args: { id: number }, context: Context) => {
+    postById: (_parent, args: { id: string }, context: Context) => {
       return context.prisma.post.findUnique({
         where: { id: args.id || undefined },
       })
     },
-    feed: (
-      _parent,
-      args: {
-        searchString: string
-        skip: number
-        take: number
-        orderBy: PostOrderByUpdatedAtInput
-      },
-      context: Context,
-    ) => {
-      const or = args.searchString
-        ? {
-            OR: [
-              { title: { contains: args.searchString } },
-              { content: { contains: args.searchString } },
-            ],
-          }
-        : {}
-
-      return context.prisma.post.findMany({
-        where: {
-          published: true,
-          ...or,
-        },
-        take: args?.take,
-        skip: args?.skip,
-        orderBy: args?.orderBy,
+    blogById: (_parent, args: { id: string }, context: Context) => {
+      return context.prisma.blog.findUnique({
+        where: { id: args.id || undefined },
       })
     },
-    draftsByUser: (
-      _parent,
-      args: { userUniqueInput: UserUniqueInput },
-      context: Context,
-    ) => {
-      return context.prisma.user
-        .findUnique({
-          where: {
-            id: args.userUniqueInput.id || undefined,
-            email: args.userUniqueInput.email || undefined,
-          },
-        })
-        .posts({
-          where: {
-            published: false,
-          },
-        })
+    blogBySlug: (_parent, args: { slug: string }, context: Context) => {
+      return context.prisma.blog.findUnique({
+        where: { slug: args.slug || undefined },
+      })
     },
   },
   Mutation: {
-    signupUser: (
-      _parent,
-      args: { data: UserCreateInput },
-      context: Context,
-    ) => {
-      const postData = args.data.posts?.map((post) => {
-        return { title: post.title, content: post.content || undefined }
-      })
-
-      return context.prisma.user.create({
+    createBlog: (_parent, args: { data: { slug: string; name: string } }, context: Context) => {
+      return context.prisma.blog.create({
         data: {
+          slug: args.data.slug,
           name: args.data.name,
-          email: args.data.email,
-          posts: {
-            create: postData,
-          },
         },
       })
     },
-    createDraft: (
-      _parent,
-      args: { data: PostCreateInput; authorEmail: string },
-      context: Context,
-    ) => {
+    createPost: (_parent, args: { data: PostCreateInput; blogId: string }, context: Context) => {
       return context.prisma.post.create({
         data: {
           title: args.data.title,
           content: args.data.content,
-          author: {
-            connect: { email: args.authorEmail },
-          },
+          blog: { connect: { id: args.blogId } },
         },
       })
     },
-    togglePublishPost: async (
-      _parent,
-      args: { id: number },
-      context: Context,
-    ) => {
-      try {
-        const post = await context.prisma.post.findUnique({
-          where: { id: args.id || undefined },
-          select: {
-            published: true,
-          },
-        })
-
-        return context.prisma.post.update({
-          where: { id: args.id || undefined },
-          data: { published: !post?.published },
-        })
-      } catch (error) {
-        throw new Error(
-          `Post with ID ${args.id} does not exist in the database.`,
-        )
-      }
-    },
-    incrementPostViewCount: (
-      _parent,
-      args: { id: number },
-      context: Context,
-    ) => {
-      return context.prisma.post.update({
-        where: { id: args.id || undefined },
-        data: {
-          viewCount: {
-            increment: 1,
-          },
-        },
-      })
-    },
-    deletePost: (_parent, args: { id: number }, context: Context) => {
-      return context.prisma.post.delete({
-        where: { id: args.id },
-      })
+  },
+  Blog: {
+    posts: (parent, _args, context: Context) => {
+      // .posts() to avoid n+1
+      return context.prisma.blog.findUnique({ where: { id: parent.id || undefined } }).posts()
     },
   },
   DateTime: DateTimeResolver,
-  Post: {
-    author: (parent, _args, context: Context) => {
-      return context.prisma.post
-        .findUnique({
-          where: { id: parent?.id },
-        })
-        .author()
-    },
-  },
-  User: {
-    posts: (parent, _args, context: Context) => {
-      return context.prisma.user
-        .findUnique({
-          where: { id: parent?.id },
-        })
-        .posts()
-    },
-  },
-}
-
-enum SortOrder {
-  asc = 'asc',
-  desc = 'desc',
-}
-
-interface PostOrderByUpdatedAtInput {
-  updatedAt: SortOrder
-}
-
-interface UserUniqueInput {
-  id?: number
-  email?: string
-}
-
-interface PostCreateInput {
-  title: string
-  content?: string
-}
-
-interface UserCreateInput {
-  email: string
-  name?: string
-  posts?: PostCreateInput[]
 }
 
 export const schema = createSchema({
